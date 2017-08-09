27e383269178d3220ee617a3d31d40f93e8ab53b
slaymance <shane.laymance@gmail.com>
2017-08-08 20:27:43 -0700

    Complete stringifyJSON.js recursion exercise

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return 'null';
+  }
+  
+  var objType = typeof obj;
+  if (objType === 'number' || objType === 'undefined' || objType === 'boolean') {
+    return `${obj}`;
+  }
+  
+  if (objType === 'string') {
+    return `"${obj}"`;
+  }
+
+  if (Array.isArray(obj)) {
+    var returnArray = [];
+    obj.forEach(function(element) {
+      returnArray.push(stringifyJSON(element));
+    });
+    return `[${returnArray}]`;
+  }
+  
+  var returnObj = [];
+  for (var key in obj) {
+    if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+      returnObj.push(`${stringifyJSON(key)}:${stringifyJSON(obj[key])}`);
+    }
+  }
+  return `{${returnObj}}`;
 };
